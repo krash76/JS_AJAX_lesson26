92d823003f4eea85fd4f676f4b6efadde3cf23cf
krash76 <krash76@mail.ru>
2022-03-31 12:53:47 +0200

    script.js changed

--- script.js
+++ script.js
@@ -5,28 +5,20 @@ const getData = () => {
   .then(response =>  response.json())
   .then(data1 => {
     console.log (data1);
-    sendData("https://jsonplaceholder.typicode.com/posts",JSON.stringify(data1))
-    .then(data2 => {
-      console.log(data2)
-    })
+    sendData(data1);
+  })
     .catch(error => {
       console.log (error)
     });
-  })
-  .catch(error => {
-    console.log(error)
-  })
+  
 }; 
 
-const sendData = (url, data) => {
-  return fetch(url, {
-    method: 'POST',
-    body: data,
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  })
-  .then(response => response.json())
+const sendData = (data) => {
+  let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.send(JSON.stringify(data));
+    xhr.onload = () => console.log(xhr.response);
 };
 
 getData();
